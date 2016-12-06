69f986e424913587adaddbe88fa24da6ecbe89d8
Alanlgf <alanlegoff78@gmail.com>
2016-12-06 22:25:00 +0100

    Ajout de l'image du header + titre

--- index.php
+++ index.php
@@ -7,17 +7,17 @@ get_header();
     <section class="container">
         <div class="row">
         <h1>Journée Projets</h1>
-            <div class="col-md-2 col-sm-2 col-xs-2">
+            <div class="col-md-2 col-sm-2 col-xs-2" style="margin-top: 30%;">
                 <div style="height: 200px; width: 200px; border-radius: 200px; border: 1px solid #444;">
                     <h6 style="text-align: center;padding: 60px 0 0 0; font-size: 50px;">IIM</h6>
                 </div>
             </div>
-            <div class="col-md-8 col-sm-8 col-xs-8">
+            <div class="col-md-8 col-sm-8 col-xs-8" style="margin-top: 30%;">
                 <div style="height: 50px; width: 50px; border-radius: 100px; border: 1px solid #444;margin:150px 0 0 50%;">
                 </div>
                 <h6 style="text-align: center; font-size: 20px; padding-left:50px; margin-bottom: 100px;"> Compte à rebours</h6>
             </div>
-            <div class="col-md-2 col-sm-2 col-xs-2">
+            <div class="col-md-2 col-sm-2 col-xs-2" style="margin-top: 30%;">
                 <div style="height: 200px; width: 200px; border-radius: 200px; border: 1px solid #444;">
                     <h6 style="text-align: center;padding: 60px 0 0 0; font-size: 50px;">ESILV</h6>
                 </div>
