36f7de8a8a078dcbde0d4a4ac543d114b2f3051d
Lucas Laurens <lucaslaurens.74@gmail.com>
2016-12-07 16:00:54 +0100

    block plan

--- index.php
+++ index.php
@@ -75,14 +75,14 @@ get_header();
 <div id="plan">
 <div class="container" >
  <div class="row">
-         <h2>Plan</h2>
          <div class="col-md-6 col-sm-12 col-xs-12">
              <div class="img_plan">
-                <img src="wp-content/themes/journ-eprojet/img/bleu.jpg">
+                <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2623.025458436362!2d2.234220316029438!3d48.89585197929128!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x47e665002d2c6665%3A0x9595552cb384bd7!2sP%C3%B4le+Universitaire+L%C3%A9onard+de+Vinci!5e0!3m2!1sfr!2sfr!4v1481122429648" width="600" height="450" frameborder="0" style="border:0" allowfullscreen></iframe>
              </div>
         </div>
 
      <div class="col-md-6 col-sm-12 col-xs-12">
+      <h2>Plan</h2>
          <p> Cumque ptinacius ut legum gnarus accusatorem flagitaret atque sollemnia, doctus id Caesar libertatemque superbiam.
              Cumque pertinacius ut legum gnarus accusatorem flagitaret atque sollemnia, doctus id Caesar libertatemque superbiam.
              Cumque pertinacius ut legum gnarus accusatorem flagitaret atque sollemnia, doctus id Caesar libertatemque superbiam.</p>
