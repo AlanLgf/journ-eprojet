6dd6c4efa851a7e2d75b9673138af43ace16b4d1
LE PAPE Anne-Hélène <annelepape2@gmail.com>
2016-12-07 21:34:12 +0100

    fin
    jai enlevé un truc qui devait pas être la mais ça va rien vous changer

--- footer.php
+++ footer.php
@@ -1,11 +1,11 @@
 <div id="footer">
 	<section class="container">
 		<div class="row">
-			<div class="col-md-8 col-sm-6 col-xs-4">
+			<div class="col-md-8 col-sm-4 col-xs-6">
 				<p>© Copyright  Pôle léonard de vinci 2016 - 2017</p>
 			</div>
 
-			<div class="col-xs-6 col-md-3 p-t-20">
+			<div class="col-md-2 col-sm-4 col-xs-6">
 				<ul class="socialbtns">
 					<li><a href="#"><img alt="facebbok" src="wp-content/themes/journ-eprojet/img/Facebook.png"></a></li>
 					<li><a href="#"><img alt="twitter" src="wp-content/themes/journ-eprojet/img/Twitter.png"></a></li>
