1d901d7b603f805ce97d73a496afe82ab2c84b85
LE PAPE Anne-Hélène <annelepape2@gmail.com>
2017-01-04 11:44:14 +0100

    footer

--- footer.php
+++ footer.php
@@ -1,15 +1,20 @@
 <div id="footer">
 	<section class="container-fluid">
 		<div class="row">
-			<div class="col-md-10 col-sm-4 col-xs-6">
+			<div class="col-md-4 col-sm-4 col-xs-6">
 				<p class="copyright">© Copyright  Pôle léonard de vinci 2016 - 2017</p>
 			</div>
 
+			<div class="col-lg-6 col-sm-4 col-md-6">
+				<ul class="associations">
+					<li><a href="#"><img alt="twitter" src="<?php echo get_template_directory_uri();?>/img/Leo-Cook.png" width="32px" height="32px"></a></li>
+					<li><a href="#"><img alt="twitter" src="<?php echo get_template_directory_uri();?>/img/PoleTech.jpg" width="32px" height="32px"></a></li>
+				</ul>
+			</div>
 			<div class="col-md-2 col-sm-4 col-xs-6">
 				<ul class="socialbtns">
-					<li><a href="#"><img alt="facebbok" src="<?php echo get_template_directory_uri();?>/img/Facebook.png"></a></li>
-					<li><a href="#"><img alt="twitter" src="<?php echo get_template_directory_uri();?>/img/Twitter.png"></a></li>
-
+					<li><a href="https://www.facebook.com/groupe.leonard.de.vinci/"><img alt="facebbok" src="<?php echo get_template_directory_uri();?>/img/Facebook.png"></a></li>
+					<li><a href="https://twitter.com/groupeldv"><img alt="twitter" src="<?php echo get_template_directory_uri();?>/img/Twitter.png"></a></li>
 				</ul>
 			</div>
 		</div>
