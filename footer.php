836fbee4d7c38f5cdbebf95c6ee0e502bf82a751
LE PAPE Anne-Hélène <annelepape2@gmail.com>
2017-01-04 19:20:17 +0100

    footer partenaires

--- footer.php
+++ footer.php
@@ -1,23 +1,28 @@
 <div id="footer">
 	<section class="container-fluid">
 		<div class="row">
-			<div class="col-md-4 col-sm-4 col-xs-6">
-				<p class="copyright">© Copyright  Pôle léonard de vinci 2016 - 2017</p>
-			</div>
-
-			<div class="col-lg-6 col-sm-4 col-md-6">
+			<div class="col-md-6 col-sm-4 col-xs-6">
+				<h3 class="footer-partenaire">Nos partenaires</h3>
 				<ul class="associations">
-					<li><a href="#"><img alt="twitter" src="<?php echo get_template_directory_uri();?>/img/Leo-Cook.png" width="32px" height="32px"></a></li>
-					<li><a href="#"><img alt="twitter" src="<?php echo get_template_directory_uri();?>/img/PoleTech.jpg" width="32px" height="32px"></a></li>
+					<li><a href="#"><img alt="leocook" src="<?php echo get_template_directory_uri();?>/img/Leo-Cook.png" width="70px" height="50px"></a></li>
+					<li><a href="#"><img alt="poletech" src="<?php echo get_template_directory_uri();?>/img/PoleTech.jpg" width="60px" height="60px"></a></li>
+					<li><a href="#"><img alt="poletech" src="<?php echo get_template_directory_uri();?>/img/Musique-Mix.png" width="60px" height="60px"></a></li>
 				</ul>
 			</div>
-			<div class="col-md-2 col-sm-4 col-xs-6">
+			<div class="col-md-6 col-sm-4 col-xs-6">
+				<h3 class="footer-rejoindre">Rejoignez nous</h3>
 				<ul class="socialbtns">
 					<li><a href="https://www.facebook.com/groupe.leonard.de.vinci/"><img alt="facebbok" src="<?php echo get_template_directory_uri();?>/img/Facebook.png"></a></li>
 					<li><a href="https://twitter.com/groupeldv"><img alt="twitter" src="<?php echo get_template_directory_uri();?>/img/Twitter.png"></a></li>
 				</ul>
 			</div>
 		</div>
+
+			<div class="col-md-12 col-sm-4 col-xs-6">
+				<div class="copyright">
+					<p class="copyright">© Copyright  Pôle léonard de vinci 2016 - 2017</p>
+				</div>
+			</div>
 	</section>
 </div>
 
