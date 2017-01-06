47a2ff7b0e14df781bdc226419089712a3e93cb6
Lucas Laurens <lucaslaurens.74@gmail.com>
2017-01-06 14:54:00 +0100

    footer placement

--- footer.php
+++ footer.php
@@ -1,26 +1,40 @@
 <div id="footer">
-	<section class="container-fluid">
+	<section class="container">
 		<div class="row">
-			<div class="col-md-6 col-sm-4 col-xs-6">
+		<div class="col-md-6">
+			<div class="col-md-12">
 				<h3 class="footer-partenaire">Nos partenaires</h3>
-				<ul class="associations">
-					<li><a href="#"><img alt="leocook" src="<?php echo get_template_directory_uri();?>/img/Leo-Cook.png" width="70px" height="50px"></a></li>
-					<li><a href="#"><img alt="poletech" src="<?php echo get_template_directory_uri();?>/img/PoleTech.jpg" width="60px" height="60px"></a></li>
-					<li><a href="#"><img alt="poletech" src="<?php echo get_template_directory_uri();?>/img/Musique-Mix.png" width="60px" height="60px"></a></li>
-				</ul>
 			</div>
-			<div class="col-md-6 col-sm-4 col-xs-6">
+			<div class="row">
+				<div class="col-md-4">
+					<a href="#"><img alt="leocook" id="li1" src="<?php echo get_template_directory_uri();?>/img/Leo-Cook.png" width="70px" height="50px"></a>
+				</div>
+				<div class="col-md-4" id="center_img">
+					<a href="#"><img alt="poletech" src="<?php echo get_template_directory_uri();?>/img/PoleTech.jpg" width="60px" height="60px"></a>
+				</div>
+				<div class="col-md-4">
+					<a href="#"><img alt="poletech" id="li2" src="<?php echo get_template_directory_uri();?>/img/Musique-Mix.png" width="60px" height="60px"></a>
+				</div>
+			</div>
+		</div>
+			<div class="col-md-6">
+			<div class="col-md-12">
 				<h3 class="footer-rejoindre">Rejoignez nous</h3>
-				<ul class="socialbtns">
-					<li><a href="https://www.facebook.com/groupe.leonard.de.vinci/"><img alt="facebbok" src="<?php echo get_template_directory_uri();?>/img/Facebook.png"></a></li>
-					<li><a href="https://twitter.com/groupeldv"><img alt="twitter" src="<?php echo get_template_directory_uri();?>/img/Twitter.png"></a></li>
-				</ul>
+			</div>
+				
+				<div class="col-md-6" id="imgduplanning">
+					<a href="https://www.facebook.com/groupe.leonard.de.vinci/" id="li1"><img alt="facebbok" src="<?php echo get_template_directory_uri();?>/img/Facebook.png"></a>
+				</div>
+				<div class="col-md-6" id="li2">
+					<a href="https://twitter.com/groupeldv"><img alt="twitter" src="<?php echo get_template_directory_uri();?>/img/Twitter.png"></a>
+				</div>
+				
 			</div>
 		</div>
 
 			<div class="col-md-12 col-sm-4 col-xs-6">
 				<div class="copyright">
-					<p class="copyright">© Copyright  Pôle léonard de vinci 2016 - 2017</p>
+					<p class="copyright">© Copyright  Pôle Léonard de Vinci 2016 - 2017</p>
 				</div>
 			</div>
 	</section>
